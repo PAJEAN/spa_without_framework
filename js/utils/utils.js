247618e87bd715bdcf68ae05d1e5fc12058e6a6a
PAJEAN <pierre-antoine.jean@mines-ales.fr>
2020-09-14 09:21:03 +0200

    Remove utils - 14/09/2020.

--- js/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export function ajax(method, url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.onload = () => resolve(xhr.responseText);
-        xhr.onerror = () => reject(xhr.statusText);
-        xhr.send();
-    });
-}
